ec214f9485c184aa96a45d7047ce6a74bbc83671
JuliaFC <mjfc@cin.ufpe.br>
2020-09-19 15:35:17 -0300

    Fix category visualization

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -29,12 +29,8 @@ def create_app(test_config=None):
             'http://127.0.0.1:3000')
         return response
 
-    '''
-  @TODO:
-  Create an endpoint to handle GET requests
-  for all available categories.
-  '''
-    @app.route('/categories')
+
+    @app.route('/categories', methods=['GET'])
     def get_categories():
         categories = Category.query.all()
         categories = [{'id': c.id, 'type': c.type} for c in categories]
@@ -43,19 +39,6 @@ def create_app(test_config=None):
             'success': True,
             'categories': categories
         })
-
-    '''
-  @TODO:
-  Create an endpoint to handle GET requests for questions,
-  including pagination (every 10 questions).
-  This endpoint should return a list of questions,
-  number of total questions, current category, categories.
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen
-  for three pages. Clicking on the page numbers should update the questions.
-  '''
 
     @app.route('/questions', methods=['GET'])
     def get_questions():
