40d0423001892dc58749a761b7d3de33cf9b4002
JuliaFC <mjfc@cin.ufpe.br>
2020-10-16 15:43:23 -0300

    Add migrations

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,16 +1,31 @@
-import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+app = Flask(__name__)
+database_name = "capstone"
+database_path = 'postgres://qbeofqzgzkxcgk:e2baa4f34be48554269dd8dc4aff6a5eda5bc81daaecb5ed34307f2ea2c206a9@ec2-52-73-199-211.compute-1.amazonaws.com:5432/df71t1phq444bi'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_path
 
-  return app
+db = SQLAlchemy(app)
 
-APP = create_app()
+from models import User
+
+@app.route('/add/')
+def webhook():
+    name = "ram"
+    email = "ram@ram.com"
+    u = User(id = id, nickname = name, email = email)
+    print("user created", u)
+    db.session.add(u)
+    db.session.commit()
+    return "user created"
+
+@app.route('/delete/')
+def delete():
+    u = User.query.get(i)
+    db.session.delete(u)
+    db.session.commit()
+    return "user deleted"
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
